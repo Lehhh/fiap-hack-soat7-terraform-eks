8c3cb86a4eaa68d6877258886af226276b4d8922
Leandro Shiniti Tacara <lst@MacBook-Pro-de-Leandro.local>
2024-10-01 22:36:56 -0300

    add cd into env prod

--- env/prod/Main.tf
+++ env/prod/Main.tf
@@ -2,3 +2,19 @@ module "prod" {
   source = "../../infra"
   cluster_name = "eks-cluster"
 }
+
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.61.0"
+    }
+  }
+  required_version = ">= 0.14.9"
+  backend "s3" {
+    bucket         = "soat7-lshiniti-infra-state"
+    key            = "terraform/eks/terraform.tfstate"
+    region         = "us-east-1"
+  }
+}
--- infra/Provider.tf
+++ infra/Provider.tf
@@ -1,31 +1,5 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.61.0"
-    }
-  }
-  backend "s3" {
-    bucket = "soat7-lshiniti-infra-state"  # Nome do bucket existente
-    key    = "terraform/eks/terraform.tfstate"
-    region = "us-east-1"
-  }
-
-  required_version = ">= 0.14.9"
-}
-
 provider "aws" {
   region  = var.region
 }
 
-data "aws_kms_alias" "existing" {
-  name = "alias/eks/eks-cluster"
-}
 
-data "aws_cloudwatch_log_group" "existing" {
-  name = "/aws/eks/eks-cluster/cluster"
-}
-
-output "log_group_name" {
-  value = data.aws_cloudwatch_log_group.existing.name
-}
